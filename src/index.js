ebfbfc8c0c5a1c4ab09036b9d59fbebcda543e60
ky5burke <ky5burke@gmail.com>
2024-02-23 22:31:43 -0600

    dom events challenge

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector('form');
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+  })
 });
+
+
